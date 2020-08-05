8f810bd27de7187f68e0f28ae4b1f43718832caa
MtMNC <morris.mukau@gmail.com>
2020-08-04 23:33:10 -0700

    Remove duplicate vote detection
    No longer needed in the new system because now each ranking is assigned to at most one entry

--- Voter.py
+++ Voter.py
@@ -5,7 +5,7 @@ class Voter():
     """
 
 
-    # how many distinct entries each Voter can vote for
+    # how many distinct Entries each Voter can vote for
     # (in other words, the number of distinct rankings the user can assign)
     ALLOWED_VOTE_COUNT = 3
 
@@ -13,13 +13,13 @@ class Voter():
     def __init__(self, name):
         self.name = name
         # Store votes in a list (indexed by ranking) to easily construct the iterator later on.
-        # For readability, self._votes[i] contains those entries that the Voter has assigned
+        # For readability, self._votes[i] contains those Entries that the Voter has assigned
         # ranking i.
         # Note that arrays are indexed from 0, but rankings are indexed from 1.
         # So, we make the array one element bigger than it needs to be and pretend the array is
         # indexed from 1, ignoring index 0.
         self._votes_by_ranking = [[] for _ in range(Voter.ALLOWED_VOTE_COUNT + 1)]
-        # Also store votes in a dictionary (keyed by entry) to easily find entry's ranks later on.
+        # Also store votes in a dictionary (keyed by entry) to easily find Entry's ranks later on.
         self._votes_by_entry = {}
         # the round when the Voter was last allocated to a new Entry
         self.round_when_last_moved = 0
@@ -27,9 +27,9 @@ class Voter():
 
     def rank(self, entry, ranking):
         """
-        Vote for (assign the given ranking to) the given entry.
-        Note duplicate votes can occur (multiple entries can be given the same ranking), but
-        multiple rankings cannot be assigned to the same entry.
+        Vote for (assign the given ranking to) the given Entry.
+        Note multiple rankings cannot be assigned to the same Entry. When this happens, only that
+        Entry's top ranking is used.
         """
 
         self._votes_by_ranking[ranking].append(entry)
@@ -50,24 +50,6 @@ class Voter():
         """
 
         return self._votes_by_entry[entry]
-
-
-    def get_duplicate_votes(self):
-        """
-        Return a dictionary representing all of the user's duplicate votes (when the Voter assigned
-        the same ranking to multiple Entries).
-        Each key is a ranking that the user assigned duplicates of, and each value is a list of
-        Entry names that received that key's ranking.
-        Returns None if the user has no duplicate votes.
-        """
-
-        duplicate_votes = {}
-
-        for ranking, entries in enumerate(self._votes_by_ranking):
-            if len(entries) > 1:
-                duplicate_votes[ranking] = [entry.name for entry in entries]
-
-        return duplicate_votes
 
 
     def __iter__(self):
