ed6a663a5b18997450b90bb39a6644f791569827
MtMNC <MtMNC@users.noreply.github.com>
2022-05-23 16:42:37 -0700

    Revert "Complete rebase"
    This reverts commit 744c710714f24c55a104b167ff1eed1c526107e8.

--- find_contest_winners_tideman.py
+++ find_contest_winners_tideman.py
@@ -1,8 +1,16 @@
+<<<<<<< HEAD:find_contest_winners_tideman.py
 from tidemancontest import TidemanContest
 
 def main():
     input_file_name = input("Enter the path to the voting data spreadsheet (made by create_voter_spreadsheet.py): ")
     contest = TidemanContest()
+=======
+from stvcontest import STVContest
+
+def main():
+    input_file_name = input("Enter the path to the voting data spreadsheet (made by create_voter_spreadsheet.py): ")
+    contest = STVContest()
+>>>>>>> 4af08aa (Rename Contest to STVContest):find_contest_winners_stv.py
     contest.populate_from_spreadsheet(input_file_name)
     output_file_name_prefix = input("Enter the prefix that the output spreadsheets will start with: ")
     num_winners = int(input("Enter the desired number of winners for the contest: "))
